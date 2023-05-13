3a2c7794e6f71eccff2f78a218fe420cfa0a015c
TomasRosaUA <tomasrosa@ua.pt>
2023-05-13 12:57:36 +0100

    bruh

--- frontend/src/Components/Header.js
+++ frontend/src/Components/Header.js
@@ -4,150 +4,158 @@ import { Link, useNavigate } from "react-router-dom";
 
 // MUI imports
 import {
-	Button,
-	Typography,
-	Grid,
-	AppBar,
-	Toolbar,
-	Menu,
-	MenuItem,
-	Snackbar,
+  Button,
+  Typography,
+  Grid,
+  AppBar,
+  Toolbar,
+  Menu,
+  MenuItem,
+  Snackbar,
 } from "@mui/material";
 
 // Contexts
 import StateContext from "../Contexts/StateContext";
 import DispatchContext from "../Contexts/DispatchContext";
-
-
+import logo from "./Assets/logo.svg";
 function Header() {
-	const navigate = useNavigate();
-	const GlobalState = useContext(StateContext);
-	const GlobalDispatch = useContext(DispatchContext);
+  const navigate = useNavigate();
+  const GlobalState = useContext(StateContext);
+  const GlobalDispatch = useContext(DispatchContext);
 
-	const [anchorEl, setAnchorEl] = React.useState(null);
-	const open = Boolean(anchorEl);
-	const handleClick = (event) => {
-		setAnchorEl(event.currentTarget);
-	};
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-	function HandleProfile() {
-		setAnchorEl(null);
-		navigate("/perfil");
-	}
+  function HandleProfile() {
+    setAnchorEl(null);
+    navigate("/perfil");
+  }
 
-	const [openSnack, setOpenSnack] = useState(false);
+  const [openSnack, setOpenSnack] = useState(false);
 
-	async function HandleLogout() {
-		setAnchorEl(null);
-		const confirmLogout = window.confirm("Tens a certeza que queres dar logout?");
-		if (confirmLogout) {
-			try {
-				const response = await Axios.post(
-					"http://localhost:8000/api-auth-djoser/token/logout/",
-					GlobalState.userToken,
-					{ headers: { Authorization: "Token ".concat(GlobalState.userToken) } }
-				);
+  async function HandleLogout() {
+    setAnchorEl(null);
+    const confirmLogout = window.confirm(
+      "Tens a certeza que queres dar logout?"
+    );
+    if (confirmLogout) {
+      try {
+        const response = await Axios.post(
+          "http://localhost:8000/api-auth-djoser/token/logout/",
+          GlobalState.userToken,
+          { headers: { Authorization: "Token ".concat(GlobalState.userToken) } }
+        );
 
-				GlobalDispatch({ type: "logout" });
-				setOpenSnack(true);
-			} catch (e) {}
-		}
-	}
+        GlobalDispatch({ type: "logout" });
+        setOpenSnack(true);
+      } catch (e) {}
+    }
+  }
 
-	useEffect(() => {
-		if (openSnack) {
-			setTimeout(() => {
-				navigate(0);
-			}, 1500);
-		}
-	}, [openSnack]);
+  useEffect(() => {
+    if (openSnack) {
+      setTimeout(() => {
+        navigate(0);
+      }, 1500);
+    }
+  }, [openSnack]);
 
-	return (
-		<AppBar position="static" style={{ backgroundColor: "black" }}>
-			<Toolbar>
-				<div style={{ marginRight: "auto" }}>
-					<Button color="inherit" onClick={() => navigate("/")}>
-						<Typography variant="h4">UAEstagios</Typography>{" "}
-					</Button>
-				</div>
-				<div>
-					<Button
-						color="inherit"
-						onClick={() => navigate("/listings")}
-						style={{ marginRight: "2rem" }}
-					>
-						<Typography variant="h6">Estagios</Typography>{" "}
-					</Button>
-					<Button
-						color="inherit"
-						style={{ marginLeft: "2rem" }}
-						onClick={() => navigate("/empresas")}
-					>
-						{" "}
-						<Typography variant="h6">Empresas</Typography>{" "}
-					</Button>
-				</div>
-				<div style={{ marginLeft: "auto", marginRight: "10rem" }}>
-					<Button
-						onClick={() => navigate("/adicionarestagio")}
-						className="adicionar-estagio"
-					>
-						Adicionar Estagio
-					</Button>
+  return (
+    <AppBar position="static" style={{ backgroundColor: "black" }}>
+      <Toolbar>
+        <div style={{ marginRight: "auto", marginLeft: "10rem" }}>
+          <Button color="inherit" onClick={() => navigate("/")}>
+            <img
+              className=""
+              src={logo}
+              style={{
+                height: "45px",
+                marginTop: "10px",
+                marginBottom: "10px",
+              }}
+            />{" "}
+          </Button>
+        </div>
+        <div>
+          <Button
+            className="botoes-navbar-hover"
+            onClick={() => navigate("/listings")}
+            style={{ marginRight: "1rem" }}
+            variant="text"
+            color="inherit"
+          >
+            Estágios
+          </Button>
+          <Button
+            className="botoes-navbar-hover"
+            color="inherit"
+            style={{ marginLeft: "1rem" }}
+            onClick={() => navigate("/empresas")}
+          >
+            Empresas
+          </Button>
+        </div>
+        <div style={{ marginLeft: "auto", marginRight: "10rem" }}>
+          <Button
+            className="botoes-navbar-hover adicionar-estagio"
+            onClick={() => navigate("/addproperty")}
+            color="inherit"
+          >
+            Adicionar Estagio
+          </Button>
 
-					{GlobalState.userIsLogged ? (
-						<Button
-							className="perfil"
-							onClick={handleClick}
-						>
-							{GlobalState.userUsername}
-						</Button>
-					) : (
-						<Button
-							className="perfil"
-							onClick={() => navigate("/login")}
-						>
-							Login
-						</Button>
-					)}
+          {GlobalState.userIsLogged ? (
+            <Button className="perfil" onClick={handleClick}>
+              {GlobalState.userUsername}
+            </Button>
+          ) : (
+            <Button
+              variant="contained"
+              style={{
+                backgroundColor: "rgb(146, 212, 0)",
+                marginLeft: "1rem",
+              }}
+              className="perfil"
+              onClick={() => navigate("/login")}
+            >
+              Login
+            </Button>
+          )}
 
-					<Menu
-						id="basic-menu"
-						anchorEl={anchorEl}
-						open={open}
-						onClose={handleClose}
-						MenuListProps={{
-							"aria-labelledby": "basic-button",
-						}}
-					>
-						<MenuItem
-							className="perfil"
-							onClick={HandleProfile}
-						>
-							Perfil
-						</MenuItem>
-						<MenuItem
-							className="perfil"
-							onClick={HandleLogout}
-						>
-							Logout
-						</MenuItem>
-					</Menu>
-					<Snackbar
-						open={openSnack}
-						message="Logout com sucesso!"
-						anchorOrigin={{
-							vertical: "bottom",
-							horizontal: "center",
-						}}
-					/>
-				</div>
-			</Toolbar>
-		</AppBar>
-	);
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            <MenuItem className="perfil" onClick={HandleProfile}>
+              Perfil
+            </MenuItem>
+            <MenuItem className="perfil" onClick={HandleLogout}>
+              Logout
+            </MenuItem>
+          </Menu>
+          <Snackbar
+            open={openSnack}
+            message="Logout com sucesso!"
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "center",
+            }}
+          />
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default Header;
--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -8,48 +8,34 @@ import { Button, Typography, Grid, AppBar, Toolbar } from "@mui/material";
 import Header from "./Header";
 
 // Assets
-import city from "./Assets/city.jpg";
+import city from "./Assets/uaBg.jpg";
 
 function Home() {
-	const [btnColor, setBtnColor] = useState("error");
+  const [btnColor, setBtnColor] = useState("error");
 
-	return (
-		<>
-			<div style={{ position: "relative" }}>
-				<img src={city} style={{ width: "100%", height: "92vh" }} />
-				<div
-					style={{
-						position: "absolute",
-						zIndex: "100",
-						top: "100px",
-						left: "20px",
-						textAlign: "center",
-					}}
-				>
-					<Typography
-						variant="h1"
-						style={{ color: "white", fontWeight: "bolder" }}
-					>
-						FIND YOUR <span style={{ color: "green" }}>NEXT PROPERTY</span> ON
-						THE LBREP WEBSITE
-					</Typography>
-					<Button href="/listings"
-						variant="contained"
-						style={{
-							fontSize: "3.5rem",
-							borderRadius: "15px",
-							backgroundColor: "green",
-							marginTop: "2rem",
-							boxShadow: "3px 3px 3px white",
-						}}
-						
-					>
-						SEE ALL PROPERTIES
-					</Button>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div
+        className="container-home"
+        style={{
+          backgroundImage: `url(${city})`,
+        }}
+      >
+        <div className="container-titulo-home">
+          <Typography id="titulo-home">
+            Encontra o teu próximo estágio
+          </Typography>
+          <Button
+            id="botao-ver-estagios-home"
+            href="/listings"
+            variant="contained"
+          >
+            Ver estágios
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
